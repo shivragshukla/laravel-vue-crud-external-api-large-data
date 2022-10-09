c094c5c510e1c3d43d4d0307eb7061c9c0b55f97
Shivrag <shivragshukla001@gmail.com>
2022-10-10 03:55:34 +0530

    Added: import packages in app.js

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav" >
+                <router-link to="/" class="nav-item nav-link" >Home </router-link>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <router-view> </router-view>
+    </div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,50 @@
 import './bootstrap';
+
+import App from './App.vue'
+import router from './router'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import Moment from 'moment'
+import Swal from 'sweetalert2'
+import { Form } from 'vform'
+
+import {
+  HasError,
+  AlertError,
+  AlertErrors,
+  AlertSuccess
+} from 'vform/src/components/bootstrap5'
+
+
+import { createApp } from 'vue';
+
+let Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+window.Swal = Swal;
+window.Toast = Toast;
+window.Form = Form;
+
+const app = createApp(App);
+
+app.config.globalProperties.$filters = {
+  	dateFormat(value) {
+  		return Moment(value).format('MMM D, YYYY');
+  	},
+  	subString(string) {
+	    return `${string.split(/\s+/).slice(0,5).join(" ")}...`;
+	}
+}
+
+app.use(router)
+app.mount("#app")
